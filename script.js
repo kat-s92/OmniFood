80e9dec6aa634b25a76b24a76423fad14a0ed953
kat-s92 <67002107+kat-s92@users.noreply.github.com>
2022-10-21 16:46:06 +0200

    Added Sticky Nav + JS, fixed access. issues

--- script.js
+++ script.js
@@ -23,4 +23,23 @@ allLinks.forEach(function (link) {
   });
 });
 
-sticky - nav;
+///// Sticky Navigation /////
+const heroSectionEl = document.querySelector(".hero-section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky-nav");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky-nav");
+    }
+  },
+  {
+    root: null, //in the viewport, null=viewport
+    threshold: 0, //will fire one hero-section is 0% in the view port, ie none
+    rootMargin: "-80px",
+  }
+);
+obs.observe(heroSectionEl);
